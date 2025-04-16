ab71cbd5b086587f5060e7dabb6106f80275d019
ZeusIA1104 <guibarcellosdaniel6@gmail.com>
2025-04-16 17:49:10 -0300

    Update zeus_app.py

--- zeus_app.py
+++ zeus_app.py
@@ -494,6 +494,7 @@ def receitas_fitness():
         "Salada de quinoa"
     ]
 # === INTERFACE: ABA "Treino" ===
+aba = st.selectbox("Escolha uma seção", ["Treino", "Dieta da Semana", "Suplementos e Receitas", "Gerar PDF"])
 if aba == "Treino":
     grupo = st.selectbox("Grupo muscular", list(treinos.keys()))
     objetivo_treino = user[7]
