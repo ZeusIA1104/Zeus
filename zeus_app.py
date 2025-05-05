f1690b5d592e1640a28a7137414ed16599e55ef5
ZeusIA1104 <guibarcellosdaniel6@gmail.com>
2025-05-04 23:31:34 -0300

    Update zeus_app.py

--- zeus_app.py
+++ zeus_app.py
@@ -442,7 +442,8 @@ treinos = {
 def gerar_treino(grupo, objetivo):
     return treinos.get(grupo, {}).get(objetivo, ["Nenhum treino disponível para essa combinação."])
 
-elif menu_principal == "Dieta da Semana":
+
+     elif menu_principal == "Dieta da Semana":
     st.subheader("Dieta Interativa - Zeus IA")
 
     dias_semana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
